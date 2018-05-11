e60b962ac614a048296cd76d79eadb3b2803424d
noffle <sww@eight45.net>
2018-05-10 22:32:24 -0700

    seems to work

--- index.js
+++ index.js
@@ -4,27 +4,34 @@ module.exports = {
   name: 'git',
   version: '1.0.0',
   manifest: {
-    read: 'source'
+    read: 'source',
+    author: 'source'
   },
   init: function (sbot, config) {
-    console.log('*** loading git-msg-log  ***')
+    console.log('*** loading git index***')
 
-    const view = sbot._flumeUse('git-ssb-web', flumeView(
+    const view = sbot._flumeUse('git-ssb', flumeView(
       1.0,
       function map (msg, seq) {
         var c = msg.value.content
         var isGitMsg = c.type in msgTypes || (c.type == 'post' && c.repo && c.issue)
         if (isGitMsg) {
-          return [msg.value.timestamp, msg.value.author]
+          return [msg.value.timestamp, msg.value.author + msg.value.timestamp]
         } else {
           return []
         }
       }
     ))
-    console.log('init FlumeView', view)
 
     return {
-      read: view.read
+      read: view.read,
+      author: function (opts) {
+        opts.gt = opts.gt || ''
+        opts.lt = opts.lt || ''
+        opts.gt = opts.id + opts.gt
+        opts.lt = opts.id + opts.lt + '~'
+        return view.read(opts)
+      }
     }
   }
 }
