48f030ee911ff4eeee2397ba8880d4e3120fb561
noffle <sww@eight45.net>
2018-05-11 21:32:33 -0700

    fix: use lex-encoding for proper range queries

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const flumeView = require('flumeview-level')
+var flumeView = require('flumeview-level')
+var lex = require('lex62')
 
 module.exports = {
   name: 'git',
@@ -16,7 +17,7 @@ module.exports = {
         var c = msg.value.content
         var isGitMsg = c.type in msgTypes || (c.type == 'post' && c.repo && c.issue)
         if (isGitMsg) {
-          return [msg.value.timestamp, msg.value.author + msg.value.timestamp]
+          return [msg.value.timestamp, msg.value.author + lex.encode(msg.value.sequence)]
         } else {
           return []
         }
@@ -26,14 +27,23 @@ module.exports = {
     return {
       read: view.read,
       author: function (opts) {
-        opts.gt = opts.gt || ''
-        opts.lt = opts.lt || ''
-        opts.gt = opts.id + opts.gt
-        opts.lt = opts.id + opts.lt + '~'
+        opts.gt = opts.gt || 0
+        opts.lt = opts.lt || 0
+        if (opts.gt === -Infinity) opts.gt = '!'
+        if (opts.lt === Infinity) opts.lt = '~'
+        opts.gt = opts.id + numberToLex(opts.gt)
+        opts.lt = opts.id + numberToLex(opts.lt) + '~'
+        console.log('query opts', opts)
         return view.read(opts)
       }
     }
   }
+}
+
+function numberToLex (n) {
+  if (n < 0) return n = 0
+  if (n === Infinity) return Number.MAX_SAFE_INTEGER
+  return lex.encode(n)
 }
 
 var msgTypes = {
